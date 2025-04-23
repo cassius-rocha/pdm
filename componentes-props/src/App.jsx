1811a8ee973884a4a087889acfcf8d91e2538888
lsitec-cassiusrocha <cassius.santos@lsitec.org.br>
2025-04-23 17:41:49 -0300

    Finaliza aula 04

--- componentes-props/src/App.jsx
+++ componentes-props/src/App.jsx
@@ -1,25 +1,79 @@
+import Pedido from "./Pedido"
+import Cartao from "./Cartao"
+import Feedback from "./Feedback"
+
 const App = () => {
-  return <div className="container border rounded mt-2">
-    <div className="row">
-      
-      <div className="col-sm-12 col-md-6 col-xxl-3">
-        <p>caixa 1</p>
-      </div>
+  const textoOK = "Já chegou!"
+  const textoNOK = "Ainda não chegou!"
+  const funcaoOK = () => alert("Agradecemos o feedback!")
+  const funcaoNOK = () => alert("Verificaremos o que aconteceu!")
+  const componenteFeedback = (
+    <Feedback 
+      textoOK={textoOK}
+      textoNOK={textoNOK}
+      funcaoOK={funcaoOK}
+      funcaoNOK={funcaoNOK}
+    />)
 
-      <div className="col-sm-12 col-md-6 col-xxl-3">
-        <p>caixa 2</p>
-      </div>
+  return (
+    <div className="container border rounded mt-2">
+      <div className="row">
+        
+        <div className="col-sm-12 col-md-6 col-xxl-3">
 
-      <div className="col-sm-12 col-md-6 col-xxl-3">
-        <p>caixa 3</p>
-      </div>
+          <Cartao cabecalho="22/04/2024">
+            <Pedido 
+              icone="headset"
+              titulo="Headset"
+              descricao="Headset falsificado"
+            />
+            {componenteFeedback}
+          </Cartao>
 
-      <div className="col-sm-12 col-md-6 col-xxl-3">
-        <p>caixa 4</p>
-      </div>
+        </div>
 
+        <div className="col-sm-12 col-md-6 col-xxl-3">
+
+          <Cartao cabecalho="22/04/2024">
+            <Pedido 
+                icone="hippo"
+                titulo="Hipopótamo"
+                descricao="Hipopótamo falsificado"
+            />
+            {componenteFeedback}
+          </Cartao>
+
+        </div>
+
+        <div className="col-sm-12 col-md-6 col-xxl-3">
+          
+          <Cartao cabecalho="22/04/2024">
+            <Pedido 
+                icone="gamepad"
+                titulo="Gamepad"
+                descricao="Gamepad falsificado"
+            />
+            {componenteFeedback}
+          </Cartao>
+
+        </div>
+
+        <div className="col-sm-12 col-md-6 col-xxl-3">
+          
+          <Cartao cabecalho="22/04/2024">
+            <Pedido 
+                icone="snowman"
+                titulo="Boneco de Neve"
+                descricao="Boneco de neve falsificado"
+            />
+            {componenteFeedback}    
+          </Cartao>
+
+        </div>
+
+      </div>
     </div>
-  </div>
+  )
 }
 
 export default App  
